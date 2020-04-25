37965d519d72b79a63c9b81a0ed32e3ff4f55e2e
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-04-25 17:30:15 -0400

    Fixed error making HTTPS API call and receiving JSON Object.

--- app/src/main/java/com/example/directstar/SearchActivity.java
+++ app/src/main/java/com/example/directstar/SearchActivity.java
@@ -3,28 +3,90 @@ package com.example.directstar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+//import com.google.android.gms.common.api.Response;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+
+
+import org.json.JSONObject;
 
 public class SearchActivity extends AppCompatActivity {
     TextView name, email, id;
     Button signOutButton;
     GoogleSignInClient mGoogleSignInClient;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        // ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://api.eventful.com/json/venues/search?app_key=62KpsJNvpqFdhZnr&keywords=Target&location=Boynton&within=10&units=mi&sort_order=popularity";
+
+        // Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Do something with the response
+//                        textView.setText(response);
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        // Handle error
+//                        textView.setText(error.toString()); //Prints error to textView
+//                    }
+//                });
+
+        // Request a json response from the provided URL.
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textView.setText(response.toString());
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        textView.setText(error.toString());
+                    }
+                });
+
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
