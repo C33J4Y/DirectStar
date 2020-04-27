ac9fa0e34797e30c2898ee6522587569e869eb64
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-04-27 17:51:22 -0400

    Added code to Parse JSON data.

--- app/src/main/java/com/example/directstar/searchResultActivity.java
+++ app/src/main/java/com/example/directstar/searchResultActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,6 +14,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class searchResultActivity extends AppCompatActivity {
@@ -34,33 +37,39 @@ public class searchResultActivity extends AppCompatActivity {
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://api.eventful.com/json/venues/search?app_key=62KpsJNvpqFdhZnr&keywords="+keyword+"&location=Boynton&within=10&units=mi&sort_order=popularity";
-
-        // Request a string response from the provided URL.
-//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        // Do something with the response
-//                        textView.setText(response);
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        // Handle error
-//                        textView.setText(error.toString()); //Prints error to textView
-//                    }
-//                });
+        String url = "https://api.eventful.com/json/venues/search?app_key=62KpsJNvpqFdhZnr&keywords=" + keyword + "&location=Boynton&within=10&units=mi&sort_order=popularity";
 
         // Request a json response from the provided URL.
-
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        searchResultView.setText(response.toString());
+                        //searchResultView.setText(response.toString());
+                        try {
+                            JSONObject venues = response.getJSONObject("venues");
+                            JSONArray venueArray = venues.getJSONArray("venue");
+                            JSONObject venuePart = venueArray.getJSONObject(0);
+                            String venueName = venuePart.getString("venue_name");
+                            String address = venuePart.getString("address");
+                            String cityName = venuePart.getString("city_name");
+                            String state = venuePart.getString("region_name");
+                            String venueType = venuePart.getString("venue_type");
+
+                            // TODO: Add IF statements to let users know if search returned NO RESULTS
+
+                                //searchResultView.setText(address);
+                            Toast.makeText(searchResultActivity.this,venueName,Toast.LENGTH_LONG).show();
+                            Toast.makeText(searchResultActivity.this,address,Toast.LENGTH_LONG).show();
+                            Toast.makeText(searchResultActivity.this,cityName,Toast.LENGTH_LONG).show();
+                            Toast.makeText(searchResultActivity.this,state,Toast.LENGTH_LONG).show();
+                            Toast.makeText(searchResultActivity.this,venueType,Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            //e.printStackTrace();
+                            searchResultView.setText(e.toString());
+                        }
+
 
                     }
                 }, new Response.ErrorListener() {
@@ -76,4 +85,50 @@ public class searchResultActivity extends AppCompatActivity {
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
     }
-}
+
+    //Function to parse json data
+//    public void displaySearchResults(JSONObject response) {
+//        String searchResults;
+//        String CRLF = "\n";
+//
+//
+//        // Capture the layout's TextView and set the string as its text
+//        final TextView searchResultView = findViewById(R.id.searchResultView);
+//
+//
+//
+//
+//            try {
+
+           // JSONObject venues = response.getJSONObject("venues");
+//            String address = venue.get("address").toString();
+//            String city = venue.get("city_name").toString();
+            //JSONArray venue = response.getJSONArray("venue");
+            //JSONObject venuePart = venue.getJSONObject(0);
+           // String venueName = venuePart.getString("venue_name");
+            //String address = venuePart.getString("address");
+            //String city = venuePart.getString("city_name");
+//            String cityName = response.getString("name");
+//            JSONObject mainJSON = response.getJSONObject("main");
+//            String temperature = mainJSON.getString("temp");
+//            searchResults = cityName + " Weather" + CRLF + CRLF +
+//                    "Longitude:\t\t\t" + lon + CRLF +
+//                    "Latitude:\t\t\t" + lat + CRLF +
+//                    "Weather:\t\t\t" + main.toUpperCase() + CRLF +
+//                    "Description:\t\t" + description.toUpperCase() + CRLF +
+//                    "Temperature:\t" + temperature;
+//        searchResults = //"Name:\t\t\t" + venueName + CRLF +
+//                        "address:\t\t\t" + address + CRLF +
+//                        "City:\t\t\t" + city;
+//        searchResultView.setText(searchResults);
+//
+//
+//          } catch (JSONException e) {
+//        e.printStackTrace();
+//         searchResultView.setText(e.toString());
+//        }
+//
+//          }
+
+    }
+
