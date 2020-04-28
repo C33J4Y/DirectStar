708845457120d9647830f86d5f11a84cd228e042
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-04-27 23:23:33 -0400

    Added displaySearchResults function.

--- app/src/main/java/com/example/directstar/searchResultActivity.java
+++ app/src/main/java/com/example/directstar/searchResultActivity.java
@@ -46,29 +46,8 @@ public class searchResultActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         //searchResultView.setText(response.toString());
-                        try {
-                            JSONObject venues = response.getJSONObject("venues");
-                            JSONArray venueArray = venues.getJSONArray("venue");
-                            JSONObject venuePart = venueArray.getJSONObject(0);
-                            String venueName = venuePart.getString("venue_name");
-                            String address = venuePart.getString("address");
-                            String cityName = venuePart.getString("city_name");
-                            String state = venuePart.getString("region_name");
-                            String venueType = venuePart.getString("venue_type");
+                        displaySearchResults(response);
 
-                            // TODO: Add IF statements to let users know if search returned NO RESULTS
-
-                                //searchResultView.setText(address);
-                            Toast.makeText(searchResultActivity.this,venueName,Toast.LENGTH_LONG).show();
-                            Toast.makeText(searchResultActivity.this,address,Toast.LENGTH_LONG).show();
-                            Toast.makeText(searchResultActivity.this,cityName,Toast.LENGTH_LONG).show();
-                            Toast.makeText(searchResultActivity.this,state,Toast.LENGTH_LONG).show();
-                            Toast.makeText(searchResultActivity.this,venueType,Toast.LENGTH_LONG).show();
-
-                        } catch (JSONException e) {
-                            //e.printStackTrace();
-                            searchResultView.setText(e.toString());
-                        }
 
 
                     }
@@ -87,48 +66,44 @@ public class searchResultActivity extends AppCompatActivity {
     }
 
     //Function to parse json data
-//    public void displaySearchResults(JSONObject response) {
-//        String searchResults;
-//        String CRLF = "\n";
-//
-//
-//        // Capture the layout's TextView and set the string as its text
-//        final TextView searchResultView = findViewById(R.id.searchResultView);
-//
-//
-//
-//
-//            try {
+    public void displaySearchResults(JSONObject response) {
+        String searchResults;
+        String CRLF = "\n";
 
-           // JSONObject venues = response.getJSONObject("venues");
-//            String address = venue.get("address").toString();
-//            String city = venue.get("city_name").toString();
-            //JSONArray venue = response.getJSONArray("venue");
-            //JSONObject venuePart = venue.getJSONObject(0);
-           // String venueName = venuePart.getString("venue_name");
-            //String address = venuePart.getString("address");
-            //String city = venuePart.getString("city_name");
-//            String cityName = response.getString("name");
-//            JSONObject mainJSON = response.getJSONObject("main");
-//            String temperature = mainJSON.getString("temp");
-//            searchResults = cityName + " Weather" + CRLF + CRLF +
-//                    "Longitude:\t\t\t" + lon + CRLF +
-//                    "Latitude:\t\t\t" + lat + CRLF +
-//                    "Weather:\t\t\t" + main.toUpperCase() + CRLF +
-//                    "Description:\t\t" + description.toUpperCase() + CRLF +
-//                    "Temperature:\t" + temperature;
-//        searchResults = //"Name:\t\t\t" + venueName + CRLF +
-//                        "address:\t\t\t" + address + CRLF +
-//                        "City:\t\t\t" + city;
-//        searchResultView.setText(searchResults);
-//
-//
-//          } catch (JSONException e) {
-//        e.printStackTrace();
-//         searchResultView.setText(e.toString());
-//        }
-//
-//          }
+
+        // Capture the layout's TextView and set the string as its text
+        final TextView searchResultView = findViewById(R.id.searchResultView);
+
+        try {
+            JSONObject venues = response.getJSONObject("venues");
+            JSONArray venueArray = venues.getJSONArray("venue");
+            JSONObject venuePart = venueArray.getJSONObject(0);
+            String venueName = venuePart.getString("venue_name");
+            String address = venuePart.getString("address");
+            String cityName = venuePart.getString("city_name");
+            String state = venuePart.getString("region_name");
+            String venueType = venuePart.getString("venue_type");
+            searchResults = "Venue Name: " + venueName + CRLF +
+                    "Address: " + address + CRLF +
+                    "City: " + cityName + CRLF +
+                    "State: " + state + CRLF +
+                    "Venue Type:\t" + venueType;
+
+            searchResultView.setText(searchResults);
+
+            // TODO: Add IF statements to let users know if search returned NO RESULTS
+            // TODO: ADD FOR loop to iterate through each object in the array to display multiple results
+
+        } catch (JSONException e) {
+            //e.printStackTrace();
+            searchResultView.setText(e.toString());
+        }
+
+
+
 
     }
+
+}
+
 
