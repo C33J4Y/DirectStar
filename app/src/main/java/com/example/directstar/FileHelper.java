104352cd0e9ad16e6aa0f790476fada4aec0b340
C33J4Y <61567416+C33J4Y@users.noreply.github.com>
2020-05-12 01:52:23 -0400

    Added helper file for FileInputStream and Input stream.

---
+++ app/src/main/java/com/example/directstar/FileHelper.java
@@ -0,0 +1,57 @@
+package com.example.directstar;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class FileHelper {
+
+    public static final String FILENAME = "listinfo3.dat";
+
+    public static void writeData(ArrayList<String> favorites, Context context){
+
+        try {
+            FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(favorites);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+
+           e.printStackTrace();
+
+            }
+        }
+
+        public static ArrayList<String> readData(Context context){
+
+            ArrayList<String> favorites = null;
+            try {
+                FileInputStream fis = context.openFileInput(FILENAME);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+                favorites = (ArrayList<String>) ois.readObject();
+            } catch (FileNotFoundException e){
+
+                favorites = new ArrayList<>();
+                e.printStackTrace();
+
+            } catch(IOException e){
+
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return favorites;
+        }
+
+}
+
+
